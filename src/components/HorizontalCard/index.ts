ed752639e8668bd2fc745f3cdf4c0c68f7bc9d13
Daniil <daniil.kobylkin.pro@gmail.com>
2023-09-12 12:47:56 +0300

    categories page

---
+++ src/components/HorizontalCard/index.ts
@@ -0,0 +1,2 @@
+export { default } from './HorizontalCard';
+export * from './HorizontalCard';
--- src/pages/CategoriesPage/CategoriesPage.tsx
+++ src/pages/CategoriesPage/CategoriesPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import styles from './CategoriesPage.module.scss';
 import Loader from 'components/Loader';
+import HorizontalCard from 'components/HorizontalCard';;
+import api from 'api';
+import { Category } from 'types';
+import { useNavigate } from "react-router-dom";
 
 const CategoriesPage: React.FC = () => {
-    const [isLoading, setLoading] = React.useState<boolean>(false);
+    const [isLoading, setLoading] = React.useState<boolean>(true);
+    const [categories, setCategories] = React.useState<Category[] | undefined>(undefined);
+    const navigate = useNavigate();
+
+    React.useEffect(() => {
+        api.fetchCategories().then(categories => {
+            setCategories(categories);
+            setLoading(false);
+        });
+    }, []);
 
     if (isLoading) {
         return (
@@ -15,9 +28,18 @@ const CategoriesPage: React.FC = () => {
 
 
     return (
-        <>
-            Категории
-        </>
+        <div className={styles['categories-wrapper']}>
+            {
+                categories.map(category => (
+                    <HorizontalCard className={styles['card']}
+                                    key={category.id}
+                                    title={category.name}
+                                    image={category.image}
+                                    onClick={() => navigate(`/?filters=${category.id}`)}
+                    />
+                ))
+            }
+        </div>
     )
 }
 
